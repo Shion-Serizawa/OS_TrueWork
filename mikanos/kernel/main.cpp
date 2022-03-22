a1266ce90377f8b20e0d8de972f5135a2191caff
Shion-Serizawa <61482303+Shion-Serizawa@users.noreply.github.com>
2022-03-22 14:37:44 +0900

    ~P85 3.5

--- mikanos/kernel/main.cpp
+++ mikanos/kernel/main.cpp
@@ -1,6 +1,16 @@
-extern "C" void KernelMain(){
-    //extern "C"はC言語形式で関数を定義することを意味する。
-    while(1) __asm__("hlt");
+#include <cstdint>
+
+extern "C" void KernelMain(uint64_t frame_buffer_base, uint64_t frame_buffer_size)
+{
+    // extern "C"はC言語形式で関数を定義することを意味する。
+
+    uint8_t *frame_buffer = reinterpret_cast<uint8_t *>(frame_buffer_base);
+    for (uint64_t i = 0; i < frame_buffer_size; ++i)
+    {
+        frame_buffer[i] = i % 256;
+    }
+
+    while (1) __asm__("hlt");
     //__asm__はCでアセンブリ言語の命令を入れ込むときに使う。
     //　hlt, 割り込み？が来るまで停止
 }
